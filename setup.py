43fb931fc8c998109965edb063c9e228920d3690
Ahnaf <ahnafzamil896@gmail.com>
2020-09-10 12:09:46 +0600

    Added package info

---
+++ setup.py
@@ -0,0 +1,27 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="zenora",
+    version="0.0.20",
+    author="K.M Ahnaf Zamil",
+    author_email="ahnafzamil@protonmail.com",
+    description="A modern Discord REST API wrapper that allows you to access the data without running a bot.",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/ahnaf-zamil/zenora",
+    packages=["zenora", "zenora.impl", "zenora.base", "zenora.utils"],
+    install_requires=[
+        "attrs",
+        "requests",
+        "typing",
+    ],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires=">=3.6",
+)
--- zenora/__init__.py
+++ zenora/__init__.py
@@ -1,2 +1,12 @@
 from zenora.impl.rest import RESTAPI
 from zenora.channels import GuildTextChannel
+
+__author__ = "K.M Ahnaf Zamil"
+__copyright__ = "© 2020 K.M Ahnaf Zamil"
+__docs__ = "https://zenora-py.github.io"
+__email__ = "ahnafzamil@protonmail.com"
+__issue_tracker__ = "https://github.com/ahnaf-zamil/zenora/issues"
+__license__ = "MIT"
+__url__ = "https://github.com/ahnaf-zamil/zenora/"
+__version__ = "0.0.20"
+__git_sha1__ = "HEAD"
